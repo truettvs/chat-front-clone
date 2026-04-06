ac89b4d620a966dafbdc202c771b0be5516fb937
truettvs <truettvs@gmail.com>
2026-04-06 10:07:01 -0500

    Fixed alias for Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'node:path'
 
 export default defineConfig({
   plugins: [react()],
   base: '/chat-front-clone/',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
