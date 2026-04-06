02418daddb0aa94819d714925b99556de7773422
truettvs <truettvs@gmail.com>
2026-04-06 10:04:45 -0500

    Github actions

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,7 @@
-import path from "node:path";
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-});
+  base: '/chat-front-clone/',
+})
